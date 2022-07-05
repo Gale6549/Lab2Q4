46a63edc45385a48b8b149a6b802048773559753
kubol <gale6549@gmail.com>
2022-07-05 15:21:34 -0700

    Initial commit

---
+++ src/Q4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of your input");
+        int inputNumber=sc.nextInt();
+        int i;
+        for(i=0;i<inputNumber;i++){
+            System.out.println("Enter your number");
+            int num=sc.nextInt();
+            int sum;
+            sum+=num;
+        }
+    }
+}
+
